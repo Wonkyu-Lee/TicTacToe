95e124d57cd6aa65927a41c058db423e31f04d3a
Wonkyu-Lee <wkyulees@gmail.com>
2017-11-29 19:48:05 +0900

    Change to play mode.

--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 
 int main()
 {
-    //tic_tac_toe::play(false);
+    tic_tac_toe::play(true);
 
-    tic_tac_toe::Game game;
-    game.mark(4);
-    game.mark(0);
-    game.mark(5);
-    game.mark(3);
-    game.mark(6);
-    game.mark(2);
+// debugging
+//    tic_tac_toe::Game game;
+//    game.mark(4);
+//    game.mark(0);
+//    game.mark(5);
+//    game.mark(3);
+//    game.mark(6);
+//    game.mark(2);
+//    tic_tac_toe::run(&game);
 
-    tic_tac_toe::run(&game);
     return 0;
 }
